3dae645f184ea18c10f9571386a249d6f92cf380
not a cat <30339395+nnucat@users.noreply.github.com>
2022-10-19 20:01:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
